dd2b83cf3851cf0cfbfd20485c64dee9142b3a29
Laravel Shift <shift@laravelshift.com>
2021-01-12 12:08:59 +0000

    Shift to class based factories

--- database/seeds/AccountsTableSeeder.php
+++ database/seeds/AccountsTableSeeder.php
@@ -9,7 +9,7 @@ class AccountsTableSeeder extends Seeder
     public function run()
     {
         User::get()->each(function (User $user) {
-            factory(Account::class, rand(1, 10))->create(['user_id' => $user->id]);
+            Account::factory()->count(rand(1, 10))->create(['user_id' => $user->id]);
         });
     }
 }
